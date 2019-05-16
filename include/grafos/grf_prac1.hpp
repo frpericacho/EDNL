f11fde8e73178f77fbfd74a9ed1252480850a0f7
frpericacho <frpericacho@gmail.com>
2019-05-16 18:03:28 +0200

    :rice_ball:

--- include/grafos/grf_prac1.hpp
+++ include/grafos/grf_prac1.hpp
@@ -13,9 +13,52 @@ using grafos::pmc::GrafoP;
     desde  cada vértice del grafo y  su correspondiente coste.
 */
 
-template<typename T>
-vector<T> DijkstraInv(const GrafoP<T> &G, typename GrafoP<T>::vertice destino, vector<typename GrafoP<T>::vertice>&P){
-    
+template <typename T>
+vector<T> DijkstraInv(const GrafoP<T> &G, typename GrafoP<T>::vertice destino, vector<typename GrafoP<T>::vertice> &P)
+{
+    typedef typename GrafoP<tCoste>::vertice vertice;
+    vertice v, w;
+    const size_t n = G.numVert();
+    vector<bool> S(n, false); // Conjunto de vértices vacío.
+    vector<tCoste> D;         // Costes mínimos desde origen.
+    // Iniciar D y P con caminos directos desde el vértice origen.
+    D = G[origen];
+    D[origen] = 0; // Coste origen-origen es 0.
+    P = vector<vertice>(n, origen);
+    S[origen] = true; // Incluir vértice origen en S.
+    for (size_t i = 1; i <= n - 2; i++)
+    { // Localizar vértice w no incluido en S con menor coste desde origen
+        tCoste costeMin = GrafoP<tCoste>::INFINITO;
+        for (v = 0; v <= n - 1; v++)
+            if (!S[v] && D[v] <= costeMin)
+            {
+                costeMin = D[v];
+                w = v;
+            }
+        S[w] = true; // Incluir vértice w en S.
+        // Recalcular coste hasta cada v no incluido en S, a través de w.
+        for (v = 0; v <= n - 1; v++)
+            if (!S[v])
+            {
+                tCoste Owv = suma(D[w], G[w][v]);
+                if (Owv < D[v])
+                {
+                    D[v] = Owv;
+                    P[v] = w;
+                }
+            }
+    }
+    return D;
+}
+
+template <typename tCoste>
+tCoste suma(tCoste x, tCoste y)
+{
+    const tCoste INFINITO = GrafoP<tCoste>::INFINITO;
+    if (x == INFINITO || y == INFINITO)
+        return INFINITO;
+    else
+        return x + y;
 }
 
 #endif
