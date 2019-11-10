54a0a7c39de0859bbd34a5233fa5b7573958efd7
frpericacho <frpericacho@gmail.com>
2019-11-10 16:57:56 +0100

    :rice_ball:

--- app/main.cpp
+++ app/main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
-#include "grafos/grf_prac2.hpp"
-#include "grafos/io.hpp"
+//#include "grafos/grf_prac2.hpp"
+//#include "grafos/io.hpp"
+#include "abin_prac1.hpp"
 
-using namespace grafos::pmc;
+
+//using namespace grafos::pmc;
 using namespace std;
+using namespace Enlazada;
 
 typedef unsigned tCoste;
 
 int main() {
-  /*
+
   Abin<int> A,B;
   A.insertarRaizB(1);
   A.insertarHijoIzqdoB(A.raizB(), 0);
   A.insertarHijoDrchoB(A.raizB(), 1);
   A.insertarHijoDrchoB(A.hijoIzqdoB(A.raizB()),0);
-  //A.insertarHijoIzqdoB(A.hijoIzqdoB(A.raizB()),17);
+  A.insertarHijoIzqdoB(A.hijoIzqdoB(A.raizB()),17);
   A.insertarHijoDrchoB(A.hijoDrchoB(A.raizB()),1);
   A.insertarHijoIzqdoB(A.hijoDrchoB(A.raizB()),0);
   A.insertarHijoIzqdoB(A.hijoDrchoB(A.hijoDrchoB(A.raizB())),1);
   A.insertarHijoDrchoB(A.hijoDrchoB(A.hijoDrchoB(A.raizB())),1);
   A.insertarHijoDrchoB(A.hijoIzqdoB(A.hijoDrchoB(A.raizB())),0);
-  //B.insertarHijoIzqdoB(B.hijoDrchoB(B.raizB()),10);
-//  cout << A.altura(A.raizB()) << endl;
-//  cout << busca(A) << endl;
+  B.insertarHijoIzqdoB(B.hijoDrchoB(B.raizB()),10);
+  cout << A.altura(A.raizB()) << endl;
+  cout << busca(A) << endl;
   std::cout << A << std::endl;
-  std::cout << rebeldes(A) << std::endl;
-  //std::cout <<
-primo(A.hijoIzqdoB(A.hijoDrchoB(A.raizB())),A.hijoDrchoB(A.hijoDrchoB(A.raizB())),A)
-<< std::endl;
-  //std::cout << nietos(A) << std::endl;
-  //std::cout << rama(A) << std::endl;
+  //std::cout << rebeldes(A) << std::endl;
+//   std::cout <<
+// primo(A.hijoIzqdoB(A.hijoDrchoB(A.raizB())),A.hijoDrchoB(A.hijoDrchoB(A.raizB())),A)
+// << std::endl;
+  // std::cout << nietos(A) << std::endl;
+  // std::cout << rama(A) << std::endl;
   //rama(A);
-  */
+  
   /*
    Agen<int> A;
    A.insertarRaiz(5);
@@ -49,10 +52,10 @@ primo(A.hijoIzqdoB(A.hijoDrchoB(A.raizB())),A.hijoDrchoB(A.hijoDrchoB(A.raizB())
       std::cout << "nope";
       */
 
-  GrafoP<short> G("files/aciclico.txt");
+  // GrafoP<short> G("files/aciclico.txt");
 
-  grafos::pmc::alg::arista<short> res = ejercicio1(G);
+  // grafos::pmc::alg::arista<short> res = ejercicio1(G);
 
-  cout << "origen: " << res.orig << "\ndestino: " << res.dest << "\ncostes: " << res.coste <<  endl;
+  // cout << "origen: " << res.orig << "\ndestino: " << res.dest << "\ncostes: " << res.coste <<  endl;
 
 }
